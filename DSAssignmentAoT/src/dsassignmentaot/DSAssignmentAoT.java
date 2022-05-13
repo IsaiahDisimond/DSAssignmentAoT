19e90310998ce9ec9004468ddde6113c792416f7
Yap-Yoong-Siew <s2122194@siswa.um.edu.my>
2022-05-13 16:25:29 +0800

    Create a java project
    Hello Github!

---
+++ DSAssignmentAoT/src/dsassignmentaot/DSAssignmentAoT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dsassignmentaot;
+
+/**
+ *
+ * @author user
+ */
+public class DSAssignmentAoT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+    }
+    
+}
