06727ea7fb13b30937428d8b3d30bee65b9380ce
Yap-Yoong-Siew <s2122194@siswa.um.edu.my>
2022-05-18 21:12:15 +0800

    Added class for Eren's Allies
    Eren's friends, this is used as a data type for each friends of Eren for the linkedList later

---
+++ DSAssignmentAoT/src/dsassignmentaot/Allies.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsassignmentaot;
+
+/**
+ *
+ * @author user
+ */
+public class Allies {
+    private String name;
+    private int height;
+    private int weight;
+    private int strength;
+    private int agility;
+    private int intelligence;
+    private int coordination;
+    private int leadership;
+
+    public Allies(String name, int height, int weight, int strength, int agility, int intelligence, int coordination, int leadership) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.coordination = coordination;
+        this.leadership = leadership;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getCoordination() {
+        return coordination;
+    }
+
+    public int getLeadership() {
+        return leadership;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setCoordination(int coordination) {
+        this.coordination = coordination;
+    }
+
+    public void setLeadership(int leadership) {
+        this.leadership = leadership;
+    }
+    
+    
+}
